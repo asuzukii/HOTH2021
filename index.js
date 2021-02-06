5266b50096ea37f6b5ea9de82966fb0ba340310d
jason2020 <jtay20@punahou.edu>
2021-02-05 19:57:08 -1000

    Setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+// const router = require("./routes.js");
+
+const app = express();
+
+// if (process.env.NODE_ENV !== "production") app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('static'))
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started.");
+});
