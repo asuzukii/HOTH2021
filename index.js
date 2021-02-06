9eabb139ed217a6b3716de810acc79bf9842b547
jason2020 <jtay20@punahou.edu>
2021-02-05 20:39:07 -1000

    Run python with index.js

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('static'))
 
+makeMeme(); // temporarily placed here
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started.");
+  
 });
+
+// TODO put arguments and stuffs (passed in from frontend)
+function makeMeme() {
+  const spawn = require("child_process").spawn; 
+  console.log("Running python");
+  const process = spawn('python', ["./randomtesting/tracking.py"]); 
+  
+  // Takes stdout data from script which executed 
+  // with arguments and send this data to res object 
+  process.stdout.on('data', function(data) { 
+      console.log(data.toString());
+  }); 
+}
