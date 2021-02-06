0f95099ba64de9c2ba94a81eed2b3fc9451f96e7
limelego <44691029+limelego@users.noreply.github.com>
2021-02-06 11:12:52 -0800

    first template

--- memetemplates/strike/createStrike.py
+++ memetemplates/strike/createStrike.py
@@ -8,17 +8,26 @@ clip = VideoFileClip("strike.mp4")
 # LATER, IN ANOTHER SCRIPT, RECOVER THESE TRAJECTORIES
 from moviepy.video.tools.tracking import Trajectory
 # If ever you only have one object being tracked, recover it with
-traj, =  Trajectory.load_list('track.txt')
+traj1, = Trajectory.load_list('strike_track_1.txt')
+traj2, = Trajectory.load_list('strike_track_2.txt')
+traj3, = Trajectory.load_list('strike_track_3.txt')
 
+usertext1 = "Pins"
+usertext2 = "Ball"
+usertext3 = "Arm"
 
 # Generate a text clip. You can customize the font, color, etc.
-txt_clip = TextClip("Test text", fontsize=70, color='white')
+txt_1 = TextClip("usertext1", fontsize=70, color='white')
+txt_2 = TextClip("usertext2", fontsize=70, color='white')
+txt_3 = TextClip("usertext3", fontsize=70, color='white')
 
 # Say that you want it to appear 10s at the center of the screen
-txt_clip = txt_clip.set_pos('center').set_duration(2)
+txt_1 = txt_1.set_pos(traj1).set_duration(5.367)
+txt_2 = txt_2.set_pos(traj2).set_duration(5.900)
+txt_3 = txt_3.set_pos(traj3).set_duration(4.600)
 
 # Overlay the text clip on the first video clip
-video = CompositeVideoClip([clip, txt_clip], size=(1280, 720)))
+video = CompositeVideoClip([clip, txt_1, txt_2, txt_3], size=(1280, 720))
 
 # Write the result to a file (many options available !)
 video.write_videofile("testOutput.mp4")
--- randomtesting/tracking.py
+++ randomtesting/tracking.py
@@ -3,6 +3,6 @@ from moviepy.video.tools.tracking import manual_tracking
 
 clip = VideoFileClip("strike.mp4")
 # manually indicate 3 trajectories, save them to a file
-trajectories = manual_tracking(clip, t1=0, t2=8, fps=10,
-                                    nobjects=1, savefile="strike_track.txt")
+trajectories = manual_tracking(clip, t1=0, t2=8, fps=2,
+                                    nobjects=1, savefile="track.txt")
 
